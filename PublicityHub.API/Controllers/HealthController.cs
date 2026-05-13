96687bf1b55bc9f3c8e5489e281173076da42ee0
veeramani.v <veeramani.v@CHPNX856>
2026-05-13 16:01:45 +0530

    Perform test run: https://publicityhub-api.onrender.com/api/health

---
+++ PublicityHub.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PublicityHub.API.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API is alive");
+        }
+    }
+}
